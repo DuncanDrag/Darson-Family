1594e4f8db7deb84b7eaeb60b518f0fe15de9e65
Duncan_Drag <ilya90232@gmail.com>
2019-11-17 19:38:22 +0300

    changes

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.testimonials__list').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        draggable: true,
+        arrows: false, 
+        slidesToShow: 1
+    });
+  });
